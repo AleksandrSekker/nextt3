595277bb20a0e0aa0d9ab53aa9490845610826c0
aleksandrsekker <oleksandr.sekker@onix-systems.com>
2023-02-05 16:04:32 +0200

    make fixes

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -5,20 +5,17 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser, faDrum} from "@fortawesome/free-solid-svg-icons";
 import {DropDown} from "../components/DropDown/DropDown";
 import {Select} from "../components/Select/Select";
-import {MenuButton} from "../components/MenuButton/MenuButton";
+import Sidebar from "./Sidebar/Sidebar";
+import Link from "next/link";
 
 const Header = () => {
   const { data: session } = useSession();
-  const [isOpen, setOpen] = React.useState(false);
 
-  const menuButtonStyle = {
-    marginLeft: "2rem"
-  };
   return (
     <header>
-      <nav className="bg-pinkHeader border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <div className="flex items-center">
+      <nav className="bg-pinkHeader h-[68px] border-gray-200 px-4 lg:px-6 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center h-full m-auto max-w-screen-xl">
+          <div className="flex items-center my-auto">
             {session?.user?.image ? <Image src={session?.user?.image} alt={'mini avatar'} width={30} height={30} className="rounded-full mr-2"/> : <FontAwesomeIcon icon={faUser} className=" text-white mr-2"/>}
             <span className="self-center text-xl font-semibold whitespace-nowrap text-white">{session?.user?.name || 'Guest'}</span>
           </div>
@@ -34,33 +31,30 @@ const Header = () => {
                 </button>
               )}
             </div>
-            <Select data={[
-              { id: 1, title: 'EN', icon: 'gb', },
-              { id: 2, title: 'ES', icon: 'es', },
-              { id: 3, title: 'DE', icon: 'de', },
-              { id: 4, title: 'FR', icon: 'fr', },
-              { id: 5, title: 'UA', icon: 'ua', },
-            ]} />
-
-            <MenuButton
-              isOpen={isOpen}
-              onClick={() => setOpen(!isOpen)}
-              strokeWidth="6"
-              color="#fff"
-              lineProps={{ strokeLinecap: "round" }}
-              transition={{ type: "spring", stiffness: 260, damping: 20 }}
-              width="24"
-              height="24"
-              style={menuButtonStyle}
-            />
+            <div className={'hidden lg:flex'}>
+              <Select data={[
+                { id: 1, title: 'EN', icon: 'gb', },
+                { id: 2, title: 'ES', icon: 'es', },
+                { id: 3, title: 'DE', icon: 'de', },
+                { id: 4, title: 'FR', icon: 'fr', },
+                { id: 5, title: 'UA', icon: 'ua', },
+              ]} />
+            </div>
+            <div className={'flex lg:hidden bg-blue-600'}>
+              <Sidebar />
+            </div>
           </div>
           <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-            <ul className="flex flex-col items-center font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <a href="#"
-                  className="text-white dark:text-white text-white hover:bg-white hover:text-black font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800"
-                  aria-current="page">Home</a>
-              </li>
+            <ul className="flex flex-col items-center font-medium lg:flex-row lg:space-x-1 lg:mt-0">
+              <Link href={'/'} className="text-white dark:text-white text-white hover:bg-white hover:text-black font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800"
+                aria-current="page" >
+                Home
+              </Link>
+              {/*<li>*/}
+              {/*  <a href="#"*/}
+              {/*    className="text-white dark:text-white text-white hover:bg-white hover:text-black font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800"*/}
+              {/*    aria-current="page">Home</a>*/}
+              {/*</li>*/}
               <DropDown title={"Mini's setup"} data={[
                 {title: 'Drums && cymbals', link: "/", icon: faDrum, id: 1},
                 {title: 'First time drum buyers', link: "/", icon: faDrum, id: 2},
@@ -71,6 +65,7 @@ const Header = () => {
                 {title: 'Lightning', link: "/", icon: faDrum, id: 7},
                 {title: 'Random fun stuff', link: "/", icon: faDrum,  id: 8},
               ]} />
+
               <li>
                 <a href="#"
                   className="text-white hover:bg-white hover:text-black font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800">Patreon</a>
@@ -79,14 +74,12 @@ const Header = () => {
                 <a href="#"
                   className="text-white hover:bg-white hover:text-black  hover:bg-gray-50 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800">Merch</a>
               </li>
-              <li>
-                <a href="#"
-                  className="text-white hover:bg-white hover:text-black  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800">Redpoint</a>
-              </li>
-              <li>
-                <a href="#"
-                  className="text-white hover:bg-white hover:text-black  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800">Contact</a>
-              </li>
+              <Link href='/redpoint'
+                className="text-white hover:bg-white hover:text-black  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800">Redpoint</Link>
+              <Link href='/contact'
+                className="text-white hover:bg-white hover:text-black  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 outline-none focus:outline-none dark:focus:ring-gray-800">Contact
+              </Link>
+
             </ul>
           </div>
         </div>
---
+++ src/Layout/Sidebar/Navigation.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { MenuItem } from "./MenuItem";
+import {faDrum, faHome, faContactCard} from "@fortawesome/free-solid-svg-icons";
+import styles from './sass/sidebar.module.scss';
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
+const menuItemData = [
+  {icon: faHome, text: 'Home', key: 0},
+  {icon: faDrum, text: `Mini's setup`, key: 1},
+  {icon: faDrum, text: 'Patreon', key: 2},
+  {icon: faDrum, text: 'Merch', key: 3},
+  {icon: faDrum, text: 'Redpoint', key: 4},
+  {icon: faContactCard, text: 'Contact', key: 4},
+]
+export const Navigation = () => (
+  <motion.ul className={styles.ul} variants={variants}>
+    {menuItemData.map(({icon, text, key}) => (
+      <MenuItem icon={icon} text={text} key={key} />
+    ))}
+  </motion.ul>
+);
+
